4f99af11cc6a45107777354276e51bca9de62060
Sébastien Fieloux <sebfie@yahoo.fr>
2014-06-24 21:12:37 +0200

    Add a selector select2 to filter data

--- src/common/search/search.js
+++ src/common/search/search.js
@@ -3,8 +3,26 @@ angular.module( 'tuto.search', [])
 .directive("search", function() {
   return {
     restrict: "AE",
-    link: function(scope, elem, attrs) {
-      console.log(elem);
+    require: "ngModel",
+    link: function($scope, elem, attrs, ngModelCtrl) {
+      elem = angular.element(elem).find("select")
+
+      elem.select2().on("change",function(event){
+        $scope.$apply(function(){
+          ngModelCtrl.$setViewValue(event.val);
+        });
+      });
+
+      ngModelCtrl.$formatters.push(function(value){
+        // Called when the value is read from the DOM
+        return value;
+      });
+
+      ngModelCtrl.$parsers.push(function(value){
+        // Call when the value of the ngModel change
+        return value;
+      });
+
     }
   };
 });
