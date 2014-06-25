9c3ca25fdd9eaa66501914806b09abe45c245ff7
Sébastien Fieloux <sebfie@yahoo.fr>
2014-06-25 10:23:02 +0200

    Add an example of the necessity of the third param (boolean) on $watch

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -18,5 +18,33 @@ angular.module( 'tuto.home', [
  */
 .controller( 'HomeCtrl', ["$scope", "Phone", function HomeController( $scope, Phone ) {
     $scope.results = Phone.query()
+
+    $scope.events = [];
+    $scope.example_of_watch_need_true = [{ test: ""} , {test2: ""}];
+
+    $scope.$watch("brand", function(newValue){
+      if (angular.isUndefined(newValue)) return;
+
+      $scope.events.push("We changed brand to " + newValue.join(","))
+    });
+
+    $scope.changeExampleOfWatchNeedTrue = function(){
+      $scope.example_of_watch_need_true.push({test3: ""});
+    };
+
+
+    $scope.$watch("example_of_watch_need_true", function(newValue){
+      if (angular.isUndefined(newValue)) return;
+
+      $scope.events.push("We watch example_of_watch_need_true change without the third param 'true'")
+    });
+
+    $scope.$watch("example_of_watch_need_true", function(newValue){
+      if (angular.isUndefined(newValue)) return;
+
+      $scope.events.push("We watch example_of_watch_need_true change using the third param 'true'")
+    }, true);
+
+
   }]
 )
